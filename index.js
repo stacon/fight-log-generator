3359e62413682f72a0ae8774585785106d993e69
Constantinos Stathis <constantinos-@hotmail.com>
2020-10-10 18:16:03 +0300

    Instead of JavaScript object we return a function that generates it

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 const getFightInfo = require("./src/components/getFightInfo").default;
 const Fighter = require("./src/components/Fighter").default;
 
-const fightInfo = getFightInfo(Fighter(), Fighter());
+const fightInfo = () => getFightInfo(Fighter(), Fighter());
 
 module.exports = fightInfo;
